fc1cc3ab357cc84edc7a111d5251d9e443da5937
duyhuynh02 <minhduyhuynh16@gmail.com>
2020-08-15 22:10:25 +0700

    First commit

---
+++ generator/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from random import choice
+
+
+
+def home(request):
+	return render(request, 'home.html')
+
+
+
+def generator(request):
+
+
+	characters= list('abcdefghijklmnopqrstuvwxyz')
+
+	if request.GET.get('uppercase'):
+		characters.extend('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+	if request.GET.get('numbers'):
+		characters.extend('1234567890')
+	if request.GET.get('special'):
+		characters.extend('!@#$%^&*()')
+
+
+	length = int(request.GET.get('length'), 12)
+	mypassword = ''
+
+
+	for c in range(length):
+		generator_password = choice(characters)
+		mypassword += generator_password
+
+
+	return render(request,  'generator.html', {'password': mypassword})
