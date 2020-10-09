2dbbb9d9b588d03232e318fcf85af1075cc3c259
duyhuynh02 <minhduyhuynh16@gmail.com>
2020-10-09 11:33:47 +0700

    update indentation

--- generator/urls.py
+++ generator/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from generator import views
 
 urlpatterns = [ 
-	path('', views.home, name='home'),
-	path('password/', views.generator, name='generator'),
+    path('', views.home, name='home'),
+    path('password/', views.generator, name='generator'),
 
 ]
--- generator/views.py
+++ generator/views.py
@@ -2,32 +2,23 @@ from django.shortcuts import render
 from random import choice
 
 
-
 def home(request):
-	return render(request, 'home.html')
-
-
+    return render(request, 'home.html')
 
 def generator(request):
+    characters= list('abcdefghijklmnopqrstuvwxyz')
 
+    if request.GET.get('uppercase'):
+        characters.extend('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    if request.GET.get('numbers'):
+        characters.extend('1234567890')
+    if request.GET.get('special'):
+        characters.extend('!@#$%^&*()')
 
-	characters= list('abcdefghijklmnopqrstuvwxyz')
-
-	if request.GET.get('uppercase'):
-		characters.extend('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-	if request.GET.get('numbers'):
-		characters.extend('1234567890')
-	if request.GET.get('special'):
-		characters.extend('!@#$%^&*()')
-
-
-	length = int(request.GET.get('length'), 12)
-	mypassword = ''
-
-
-	for c in range(length):
-		generator_password = choice(characters)
-		mypassword += generator_password
-
-
-	return render(request,  'generator.html', {'password': mypassword})
+    length = int(request.GET.get('length'), 12)
+    mypassword = ''
+    for c in range(length):
+        generator_password = choice(characters)
+        mypassword += generator_password
+        
+    return render(request,  'generator.html', {'password': mypassword})
